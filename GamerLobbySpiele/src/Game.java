67664b80b5086e1bce55cf44b5e4b0e73d4f40e4
Nullkisir <kevin.scheurer01@gmx.de>
2023-04-12 18:06:14 +0200

    firstTest

---
+++ GamerLobbySpiele/src/Game.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Game {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        //Spielart bestimmen
+        System.out.println("Welches Spiel wollt ihr spielen?");
+        System.out.println("1: Discord Zitat; 2: Lol Zitat; 3: Fakten Spiel; 4: Schaetzfragen(geht noch nicht) \n");
+
+        int Spielart = scan.nextInt();
+
+       /* switch(Spielart){
+            case 1:dcZitatGame.zitatGame();
+                break;
+            case 2:LolZitatGame.zitatGame();
+                break;
+            case 3:FaktenSpiel.faktenGame();
+                break;
+            case 4:SchaetzSpiel.schaetzGame();
+                break;  */
+        }
+    }
